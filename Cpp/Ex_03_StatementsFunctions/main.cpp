5f946a28aab18cce86a78e0ae3a7611d7e7f1d47
Merv <50325962+monkshoodtv@users.noreply.github.com>
2022-03-01 20:28:37 +0000

    Create README.md

--- Cpp/Ex_03_StatementsFunctions/main.cpp
+++ Cpp/Ex_03_StatementsFunctions/main.cpp
@@ -4,24 +4,19 @@ using namespace std;
 
 // A function 'addition' that is called from main
 int addition(int first_param, int second_param) {
-
 	int result = first_param + second_param;
 	return result;
 }
 
 // The main function
 int main() {
-
-	int first{5};	//A statement
-	int second{7};	//A statement
-
+	int first{5}; //A statement
+	int second{7}; //A statement
 	cout << endl << "First: " << first << endl;
 	cout << "Second: " << second << endl;
 
-	int sum = first + second;	//Using the 'main' function
-	cout << "Main Result is: " << sum << endl << endl;
-
-	cout << "Funct result is: " << addition(30, 10) << endl;	//Using the 'addition' function
-
+	int sum = first + second; //Using the 'main' function
+	cout << "Main Result is: " << sum << endl;
+	cout << "Funct result is: " << addition(30, 10) << endl; //Using the 'addition' function
 	return 0;
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# VSCode
+This is me trying out Git and learning code.
+Nothing interesting here - honest!.

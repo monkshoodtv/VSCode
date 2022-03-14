af9477e5bf34a9ab07766d903ca553f95fd3a03e
monkshoodtv <monkshoodtv@gmail.com>
2022-03-14 13:41:36 +0000

    220314

--- Python/ex_01/main.py
+++ Python/ex_01/main.py
@@ -1,1 +1,6 @@
-print("Python rules!")
+timeCalculation = 24*60*60
+timeUnit="seconds."
+
+print(f"1 day has {1 * timeCalculation} {timeUnit}")
+print(f"28 days has {28 * timeCalculation} {timeUnit}")
+print(f"1 year has {365 * timeCalculation} {timeUnit}")
